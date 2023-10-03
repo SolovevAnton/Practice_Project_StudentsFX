e2d5820afddf6390f1f937b38f462ef53c3874d1
Solovev Anton <solovev.anton93@gmail.com>
2023-10-03 17:04:17 +0200

    added student form controller

---
+++ src/main/java/com/solovev/controllers/StudentFormController.java
@@ -0,0 +1,16 @@
+package com.solovev.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class StudentFormController {
+    public TextField nameField;
+    public TextField numField;
+    public TextField salaryField;
+
+    public void saveButton(ActionEvent actionEvent) {
+    }
+
+    public void declineButton(ActionEvent actionEvent) {
+    }
+}
