e9376fd2d35b36ac149305f3a4a3061782c1bfd1
Solovev Anton <solovev.anton93@gmail.com>
2023-10-04 17:59:51 +0200

    modify and add button support

--- src/main/java/com/solovev/controllers/MainController.java
+++ src/main/java/com/solovev/controllers/MainController.java
@@ -11,9 +11,11 @@ import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
+import java.beans.EventHandler;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MainController {
     @FXML
@@ -28,24 +30,29 @@ public class MainController {
     public TableColumn<Student, Integer> columnNum;
     @FXML
     public TableColumn<Student, Double> columnSalary;
+    private StudentRepository studentRepo;
 
     public void initialize() throws IOException {
         //initialize columns
         initializeTableColumns();
 
         //add items
-        List<Student> students = new ArrayList<>(new StudentRepository().takeData());
-        studentsTable.setItems(FXCollections.observableList(students));
-
+        reloadTableValues();
 
         //initialize muse click action
         initializeMouseDoubleClickActionOnTable();
     }
 
+    private void reloadTableValues() throws IOException {
+        studentRepo = new StudentRepository();
+        List<Student> students = new ArrayList<>(studentRepo.takeData());
+        studentsTable.setItems(FXCollections.observableList(students));
+    }
+
     /**
      * Initializes table columns
      */
-    private void initializeTableColumns() {
+    private void initializeTableColumns() throws IOException {
         //initializes columns
         columnId.setCellValueFactory(new PropertyValueFactory<>("id"));
         columnName.setCellValueFactory(new PropertyValueFactory<>("name"));
@@ -55,9 +62,18 @@ public class MainController {
 
         createAndAddButtonsColumn();
     }
-    private void createAndAddButtonsColumn(){
+
+    private void createAndAddButtonsColumn() throws IOException {
         TableColumnBuilder<Student> tableColumnBuilder = new TableColumnBuilder<>();
-        tableColumnBuilder.addButtonToColumn(this::modifyButtonFactory);
+
+        tableColumnBuilder.addButtonToColumn(() -> {
+            try {
+                return modifyButtonFactory();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
         tableColumnBuilder.addButtonToColumn(this::deleteButtonFactory);
 
         //crete header
@@ -65,31 +81,50 @@ public class MainController {
 
         studentsTable.getColumns().add(tableColumnBuilder.getColumnWithButtons());
     }
-    private Button addButtonFactory(){
+
+    private Button addButtonFactory() throws IOException {
         Button addButton = new Button("+ Add");
         addButton.getStyleClass().add("success");
         addButton.setAlignment(Pos.CENTER_RIGHT);
+
+        Student studentToSave = new Student();
+        boolean saveData = addButtonBehaviorAndStudentToChange(studentToSave, addButton);
+        if (saveData) {
+            studentRepo.add(studentToSave);
+        }
         return addButton;
     }
 
-    private Button deleteButtonFactory(){
+    private Button modifyButtonFactory() throws IOException {
+        Button modifyButton = getButtonTemplate("Modify");
+        modifyButton.getStyleClass().add("accent");
+
+        Student studentToSave = getSelectedItem();
+        boolean saveData = addButtonBehaviorAndStudentToChange(studentToSave, modifyButton);
+        if (saveData) {
+            studentRepo.add(studentToSave);
+        }
+        return modifyButton;
+    }
+
+    private boolean addButtonBehaviorAndStudentToChange(Student studentToChange, Button buttonToStudentForm) throws IOException {
+        AtomicBoolean saveData = new AtomicBoolean(false);
+        buttonToStudentForm.setOnAction((event) -> saveData.set(FormsManager.openStudentChangeForm(studentToChange)));
+        return saveData.get();
+    }
+
+    private Button deleteButtonFactory() {
         Button deleteButton = getButtonTemplate("Delete");
         deleteButton.getStyleClass().add("danger");
         //todo add func
         deleteButton.setOnAction(event -> System.out.println("deleted"));
         return deleteButton;
     }
-    private Button modifyButtonFactory(){
-        Button modifyButton = getButtonTemplate("Modify");
-        modifyButton.getStyleClass().add("accent");
-        //todo add func
-        modifyButton.setOnAction(event -> System.out.println("modified"));
-        return modifyButton;
-    }
-    private Button getButtonTemplate(String buttonName){
+
+    private Button getButtonTemplate(String buttonName) {
         Button button = new Button(buttonName);
         button.setAlignment(Pos.CENTER);
-        button.getStyleClass().addAll("button-outlined","small");
+        button.getStyleClass().addAll("button-outlined", "small");
 
         return button;
     }
@@ -101,9 +136,13 @@ public class MainController {
     private void initializeMouseDoubleClickActionOnTable() {
         studentsTable.setOnMouseClicked(event -> {
             if (event.getClickCount() == 2) {
-                FormsManager.openCarsForm(studentsTable.getSelectionModel().getSelectedItem());
+                FormsManager.openCarsForm(getSelectedItem());
             }
         });
+    }
+
+    private Student getSelectedItem() {
+        return studentsTable.getSelectionModel().getSelectedItem();
     }
 
 }
--- src/main/java/com/solovev/controllers/StudentChangeFormController.java
+++ src/main/java/com/solovev/controllers/StudentChangeFormController.java
@@ -1,10 +1,12 @@
 package com.solovev.controllers;
 
+import com.solovev.model.Student;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
-public class StudentChangeFormController {
+public class StudentChangeFormController implements ControllerRetrieveData<Boolean>, ControllerData<Student>{
+    private Boolean saveData = false;
     @FXML
     public TextField nameField;
     @FXML
@@ -16,5 +18,15 @@ public class StudentChangeFormController {
     }
     @FXML
     public void declineButton(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void initData(Student value) {
+
+    }
+
+    @Override
+    public Boolean retrieveData() {
+        return saveData;
     }
 }
